2c9346e72c4a99d003e634226499be6c9620c002
amal <amalramachandran8@gmail.com>
2026-06-25 20:45:26 +0530

    npm package installed,test tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
+import { bunny,google } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -9,8 +9,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
+                // bunny('Instrument Sans', {
+                //     weights: [400, 500, 600],
+                // }),
+                google('Noto Sans Grantha', {         
+                    weights: [400],                   
+                    alias: 'brand',                  
                 }),
             ],
         }),
